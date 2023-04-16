48a828c212a9697cfa1c15adcd6db73dc01d5e07
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-04-16 20:34:45 +0500

    js update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,15 @@
 $(".marquee").marquee({
-  //speed in milliseconds of the marquee
-  duration: 9000,
-  //gap in pixels between the tickers
+  duration: 10000,
   gap: 0,
-  //time in milliseconds before the marquee will start animating
   delayBeforeStart: 0,
-  //'left' or 'right'
   direction: "left",
-  //true or false - should the marquee be duplicated to show an effect of continuous flow
   duplicated: true,
+
 });
 $(".marquee-reverse").marquee({
-  //speed in milliseconds of the marquee
-  duration: 9000,
-  //gap in pixels between the tickers
+  duration: 11000,
   gap: 0,
-  //time in milliseconds before the marquee will start animating
   delayBeforeStart: 0,
-  //'left' or 'right'
   direction: "right",
-  //true or false - should the marquee be duplicated to show an effect of continuous flow
   duplicated: true,
 });
